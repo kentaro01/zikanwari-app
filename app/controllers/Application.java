d15a57fd7796470cdf4c9c29608a7c875bfdf96e
kentaro01 <sgwqn234@gmail.com>
2014-08-19 16:59:17 +0900

    change

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,7 @@ import views.html.*;
 public class Application extends Controller {
 
 	public static Result index() {
-        return ok("時間割.");
+        return ok(index.render("Your new application is ready."));
     }
 	 public static Result tasks() {
 		    return TODO;
