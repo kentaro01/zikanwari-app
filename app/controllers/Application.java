4ed2f4ff59e50fe11e531d587bda695af4fec11e
kentaro01 <sgwqn234@gmail.com>
2014-08-20 14:41:55 +0900

    ass

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,20 +6,12 @@ import play.mvc.*;
 import views.html.*;
 public class Application extends Controller {
 
+	
+	
 	public static Result index() {
-        return ok(index.render("Your new application is ready."));
+		return ok("時間割");
     }
-	 public static Result tasks() {
-		    return TODO;
-		  }
-		  
-		  public static Result newTask() {
-		    return TODO;
-		  }
-		  
-		  public static Result deleteTask(Long id) {
-		    return TODO;
-		  }
+	
 		  
 
 }
