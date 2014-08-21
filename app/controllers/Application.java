889666af16c59295613a290e95e4069fa7dcbbe9
kentaro01 <sgwqn234@gmail.com>
2014-08-21 13:02:09 +0900

    var7

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import static play.data.Form.*;
 import views.html.*;
 public class Application extends Controller {
 
@@ -12,7 +12,10 @@ public class Application extends Controller {
 		
 		return ok(index.render("時間割", 5600));
     }
-	
+	public static Result create() {
+		Form<Computer> computerForm = form(Computer.class);
+		return ok(createForm.render(computerForm));
+	}
 	
 
 }
