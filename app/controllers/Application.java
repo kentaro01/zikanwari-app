bc1bd8c4b6856fe52f3cec25cd5b37554fe4cd7d
kentaro01 <sgwqn234@gmail.com>
2014-08-22 12:53:36 +0900

    8.22

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,38 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-import static play.data.Form.*;
-import views.html.*;
+import static play.data.Form.form;
+
+import java.util.List;
+
+import models.SampleData;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.createForm;
+import views.html.index;
+
 public class Application extends Controller {
 
-	
-	
 	public static Result index() {
-		
-		return ok(index.render("時間割", 5600));
-    }
-	
-	
+		List<SampleData> data = SampleData.find.all();
+		return ok(index.render("時間割", 5600, data));
+	}
+
+	public static Result create() {
+		Form<SampleData> dataform = form(SampleData.class);
+		return ok(createForm.render("時間割", dataform));
+	}
+
+	public static Result save() {
+		Form<SampleData> dataform = form(SampleData.class).bindFromRequest();
+		if (dataform.hasErrors()) {
+			return badRequest(createForm.render("時間割", dataform));
+		}
+		dataform.get().save();
+		flash("success");
+		return home;
+	}
+
+	public static Result home = redirect(routes.Application.index());
 
 }
